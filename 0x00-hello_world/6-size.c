5aa7f331611d1179efceff1d32cffdb3aa17cac9
brian mbithi <brianmbithi2014@gmail.com>
2022-04-07 16:23:59 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-ZZ/**
+/**
  * main - Prints out the sizes of various data types on the computer
  * this program is compiled on.
  * Return: 0 if success. 
@@ -18,4 +18,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll);
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-	}
+
+	}           
