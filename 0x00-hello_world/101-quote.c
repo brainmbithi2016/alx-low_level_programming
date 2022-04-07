535540255251859c2ef046e9be9b99b6df471e2e
brian mbithi <brianmbithi2014@gmail.com>
2022-04-07 16:38:31 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Return:always 1(success)
+ */
+int main (void)
+
+{
+   char str1[] ="and that piece of art is useful\" -Dora Korpar, 2015-10-19\n":
+	   write(2, strl, );
+   return (1)
+}
