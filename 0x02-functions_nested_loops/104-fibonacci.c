8ae99d6b8b48363533cd8a666c990639917fb588
brian mbithi <brianmbithi2014@gmail.com>
2022-04-12 17:25:36 -0700

    Task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	unsigned long int i;
 	unsigned long int bef = 1;
-	unsigned long int aft = 2:
+	unsigned long int aft = 2;
 		unsigned long int 1 = 1000000000;
 	unsigned long int bef1;
-	unsigned long unt bef2;
+	unsigned long int bef2;
 unsigned long int aft1;
 unsigned long int aft2;
 
