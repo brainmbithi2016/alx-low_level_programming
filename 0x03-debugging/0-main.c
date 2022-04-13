ea44c2b041fe8cff5dde81b48592bbb7b0d7c5ca
brian mbithi <brianmbithi2014@gmail.com>
2022-04-13 04:50:51 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
-/**
- * main -  prints if integer positive or negative
+/***
+ * main - main
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
-	i = 0;
-	positive_or_negative(i);
+int i;
+i = 0;
 
-	return (0);
+positive_or_negative(i);
+
+return (0);
 }
