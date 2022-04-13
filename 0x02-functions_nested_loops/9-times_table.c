ef919e1929717a7f0676efa2f91ff9a6e0f59239
brian mbithi <brianmbithi2014@gmail.com>
2022-04-13 00:52:02 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,44 +5,34 @@
  */
 void times_table(void)
 {
-	int a;
+	int a = 0;
 	int b;
-	int c;
-
-	for (a = 0; a <= 9; a++)
+	int rep;
+	while (a <= 9)
 	{
-		for (b = 0; b <= 9; b++)
+		rep = a * b;
+		if (b == 0)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-				_putchar ('0');
-				}
-			if (b != 0)
-			{
-			_putchar (' ');
-		_putchar((c % 10) + '0');
-			}
-		if (b < 9)
-		{
-		_putchar(',');
-	_putchar(' ');
+			_putchar('0' + rep);
 		}
-			}
-			else
-			{
-			_putchar(',');
-		_putchar((c / 10) + '0');
-		_putchar((c % 10) + '0');
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
 		if (b < 9)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-			}
-		}
-		_putchar('\n');
+		b++;
 	}
+	_putchar('\n');
+	a++;
 }
+
